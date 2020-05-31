69912861ad3bff2c7b3e434164fd9ad87ead1bb8
Jayant Rai <jayant.chamling@outlook.com>
2020-05-30 20:33:45 -0500

    Initial commit

---
+++ src/test/java/com/jayant/rentalcloud/Oauth/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayant.rentalcloud.Oauth.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
